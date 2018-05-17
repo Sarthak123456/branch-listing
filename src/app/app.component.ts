02ae15e6675648981324945b4904f1680a3d7008
Sarthak Tuteja <sarthaktuteja@Sarthaks-MacBook-Air.local>
2018-05-17 13:38:48 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { headerComponent } from '.app/header/app.headerComponent';
+import { navComponent } from '.app/nav/app.navComponent';
+import { contentAreaComponent } from '.app/contentArea/app.contentAreaComponentComponent';
+import { footerComponent } from '.app/footer/app.footerComponent';
+
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './main.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First  App!';
+  
 }
---
+++ src/app/nav/app.navComponent.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'nav',
+  templateUrl: './nav.html',
+  //styleUrls: ['./app.component.css']
+})
+export class navComponent {
+ 
+}
